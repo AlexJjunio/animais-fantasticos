3e23b97f399d241799a648f05d465fc448b92cdc
iJjunio <alex_jjunio@outlook.com>
2023-04-07 10:12:30 -0300

    Created initTabNav and initAccordion functions

---
+++ script.js
@@ -0,0 +1,44 @@
+function initTabNav() {
+  const tabContent = document.querySelectorAll('.js-tabcontent section')
+  const tabMenu = document.querySelectorAll('.js-tabmenu li')
+  tabContent[0].classList.add('ativo')
+
+  if (tabMenu.length && tabContent.length) {
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener('click', () => {
+        activeTab(index)
+      })
+    })
+
+    function activeTab(index) {
+      tabContent.forEach((section) => {
+        section.classList.remove('ativo')
+      })
+      tabContent[index].classList.add('ativo')
+    }
+  }
+}
+
+initTabNav()
+
+function initAccordion() {
+  const accordionList = document.querySelectorAll('.js-accordion dt')
+  const activeClass = 'ativo'
+
+  if (accordionList.length) {
+    accordionList[0].classList.add(activeClass)
+    accordionList[0].nextElementSibling.classList.add(activeClass)
+    function activeAccordion() {
+      this.classList.toggle(activeClass)
+      this.nextElementSibling.classList.toggle(activeClass)
+    }
+
+    accordionList.forEach((item) => {
+      item.addEventListener('click', activeAccordion)
+    })
+  }
+
+}
+
+initAccordion()
+
