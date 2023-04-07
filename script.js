4b1ddab5720f788a483409da739bf56d9d97d186
iJjunio <alex_jjunio@outlook.com>
2023-04-07 11:09:19 -0300

    Added initAnimacaoScroll function

--- script.js
+++ script.js
@@ -42,21 +42,21 @@ function initAccordion() {
 
 initAccordion()
 
-function initScrollSuave() {
+function initScrollSmooth() {
   const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]')
 
   function scrollToSection(event) {
     event.preventDefault()
     const href = event.currentTarget.getAttribute('href');
     const section = document.querySelector(href)
-    
+
     section.scrollIntoView({
       behavior: 'smooth',
       block: 'start'
     })
-    
+
     // forma alternativa
-    
+
     // const topo = section.offsetTop
     //   window.scrollTo({
     //     top: topo,
@@ -69,5 +69,31 @@ function initScrollSuave() {
   })
 }
 
-initScrollSuave()
+initScrollSmooth()
 
+function initAnimacaoScroll() {
+  const sections = document.querySelectorAll('.js-scroll')
+
+  if (sections.length) {
+    const windowMetade = window.innerHeight * 0.6;
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const inSectionVisible = (sectionTop - windowMetade) < 0
+
+        if (inSectionVisible) {
+          section.classList.add('ativo')
+        }
+      })
+    }
+  }
+
+
+
+  animaScroll()
+
+  window.addEventListener('scroll', animaScroll)
+}
+
+initAnimacaoScroll()
